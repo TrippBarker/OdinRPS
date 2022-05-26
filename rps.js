811555ff7a8987f43d45f596eacb1c052852cef1
Tripp Barker <colemanbarker@gmail.com>
2022-05-26 00:51:22 -0400

    Added scoreboard and css changes

--- rps.js
+++ rps.js
@@ -11,6 +11,12 @@ const lkrjGif = document.querySelector('#lkrj')
 const lkrpGif = document.querySelector('#lkrp')
 const lprkGif = document.querySelector('#lprk')
 const lprjGif = document.querySelector('#lprj')
+const human = document.querySelector('#blueScore');
+const tie = document.querySelector('#tieScore');
+const computer = document.querySelector('#redScore');
+let humScore = 0;
+let tieScore = 0;
+let compScore = 0;
 
 function computerWep(){
     return Math.round(Math.random() * 2);
@@ -20,22 +26,31 @@ function playRound(e){
     compWep = weapons[computerWep()];
     humWep = this.id;
         if(humWep === 'jump' && compWep === 'jump'){
+            tieScore++;
             runGif(ssGif, bjGif);
         } else if(humWep === 'kick' && compWep === 'kick'){
+            tieScore++;
             runGif(ssGif, bkGif);
         } else if(humWep === 'punch' && compWep === 'punch'){
+            tieScore++;
             runGif(ssGif, bpGif)
         } else if (humWep === 'jump' && compWep === 'kick'){
+            humScore++;
             runGif(ssGif, ljrkGif);
         } else if (humWep === 'jump' && compWep === 'punch'){
+            compScore++;
             runGif(ssGif, ljrpGif)
         } else if (humWep === 'kick' && compWep === 'jump'){
+            compScore++;
             runGif(ssGif, lkrjGif);
         } else if (humWep === 'kick' && compWep === 'punch'){
+            humScore++;
             runGif(ssGif, lkrpGif);
         } else if (humWep === 'punch' && compWep === 'kick'){
+            compScore++;
             runGif(ssGif, lprkGif);
         } else if (humWep === 'punch' && compWep === 'jump'){
+            humScore++;
             runGif(ssGif, lprjGif);
         } else {
             console.log(`Computer chose ${compWep} and human chose ${humWep}, computer wins!`);
@@ -51,6 +66,10 @@ function runGif(gif1, gif2){
     {gif1.classList.toggle('hideGif'); 
     gif2.classList.toggle('hideGif'); 
     btns.forEach(button => button.classList.toggle('disableBtn'));}, 980);
+    human.textContent = `BLUE: ${humScore}`;
+    tie.textContent = `TIE: ${tieScore}`;
+    computer.textContent = `RED: ${compScore}`;
+
 }
 
 function logText(e){
