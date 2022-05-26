2937f8b0ebd70399f2b228d57a780a180adb0420
Tripp Barker <colemanbarker@gmail.com>
2022-05-26 01:33:00 -0400

    Added function to play again

--- rps.js
+++ rps.js
@@ -11,6 +11,7 @@ const lkrjGif = document.querySelector('#lkrj')
 const lkrpGif = document.querySelector('#lkrp')
 const lprkGif = document.querySelector('#lprk')
 const lprjGif = document.querySelector('#lprj')
+const playAgainMsg = document.querySelector('#playAgain');
 const human = document.querySelector('#blueScore');
 const tie = document.querySelector('#tieScore');
 const computer = document.querySelector('#redScore');
@@ -25,36 +26,50 @@ function computerWep(){
 function playRound(e){
     compWep = weapons[computerWep()];
     humWep = this.id;
-        if(humWep === 'jump' && compWep === 'jump'){
-            tieScore++;
-            runGif(ssGif, bjGif);
-        } else if(humWep === 'kick' && compWep === 'kick'){
-            tieScore++;
-            runGif(ssGif, bkGif);
-        } else if(humWep === 'punch' && compWep === 'punch'){
-            tieScore++;
-            runGif(ssGif, bpGif)
-        } else if (humWep === 'jump' && compWep === 'kick'){
-            humScore++;
-            runGif(ssGif, ljrkGif);
-        } else if (humWep === 'jump' && compWep === 'punch'){
-            compScore++;
-            runGif(ssGif, ljrpGif)
-        } else if (humWep === 'kick' && compWep === 'jump'){
-            compScore++;
-            runGif(ssGif, lkrjGif);
-        } else if (humWep === 'kick' && compWep === 'punch'){
-            humScore++;
-            runGif(ssGif, lkrpGif);
-        } else if (humWep === 'punch' && compWep === 'kick'){
-            compScore++;
-            runGif(ssGif, lprkGif);
-        } else if (humWep === 'punch' && compWep === 'jump'){
-            humScore++;
-            runGif(ssGif, lprjGif);
-        } else {
-            console.log(`Computer chose ${compWep} and human chose ${humWep}, computer wins!`);
-        }
+    if(humWep === 'jump' && compWep === 'jump'){
+        tieScore++;
+        runGif(ssGif, bjGif);
+    } else if(humWep === 'kick' && compWep === 'kick'){
+        tieScore++;
+        runGif(ssGif, bkGif);
+    } else if(humWep === 'punch' && compWep === 'punch'){
+        tieScore++;
+        runGif(ssGif, bpGif)
+    } else if (humWep === 'jump' && compWep === 'kick'){
+        humScore++;
+        runGif(ssGif, ljrkGif);
+    } else if (humWep === 'jump' && compWep === 'punch'){
+        compScore++;
+        runGif(ssGif, ljrpGif)
+    } else if (humWep === 'kick' && compWep === 'jump'){
+        compScore++;
+        runGif(ssGif, lkrjGif);
+    } else if (humWep === 'kick' && compWep === 'punch'){
+        humScore++;
+        runGif(ssGif, lkrpGif);
+    } else if (humWep === 'punch' && compWep === 'kick'){
+        compScore++;
+        runGif(ssGif, lprkGif);
+    } else if (humWep === 'punch' && compWep === 'jump'){
+        humScore++;
+        runGif(ssGif, lprjGif);
+    } else if (humWep === 'yes'){
+        humScore = 0;
+        tieScore = 0;
+        compScore = 0;
+        btns.forEach(button => button.classList.toggle('hideGif'));
+        runGif(ssGif, ssGif);
+    } else if (humWep === 'no'){
+        btns.forEach(button => button.classList.add('hideGif'));
+        btns.forEach(button => button.classList.toggle('hideGif'));
+    } else {
+        console.log("Something happened that I did not predict!");
+    }
+    if(humScore === 5){
+        gameEnd('BLUE');
+    } else if(compScore === 5){
+        gameEnd('RED');
+    }
 }
 
 function runGif(gif1, gif2){
@@ -72,8 +87,10 @@ function runGif(gif1, gif2){
 
 }
 
-function logText(e){
-    console.log(this.classList.value);
+function gameEnd(winner){
+    playAgainMsg.textContent = `${winner} wins! Play Again?`;
+    playAgainMsg.classList.toggle('hideGif');
+    btns.forEach(button => button.classList.toggle('hideGif'));
 }
 
 btns.forEach(button => button.addEventListener('click', playRound));
